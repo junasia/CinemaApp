99ac8b490e04a22adc1de16ad0bc831267ea9a95
Filip <filip.k.wanat@gmail.com>
2019-10-01 16:24:46 +0200

    Updates

---
+++ precache-manifest.09cd73f18cab2a7cdc0a5fe13792100c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961e5353edc4bb11d5ab6fd930aa3f9d",
+    "url": "/CinemaApp/index.html"
+  },
+  {
+    "revision": "2263f4f5cadc4e762114",
+    "url": "/CinemaApp/static/css/main.4a4d62d2.chunk.css"
+  },
+  {
+    "revision": "a3eceacf1257b882ac09",
+    "url": "/CinemaApp/static/js/2.d5152bde.chunk.js"
+  },
+  {
+    "revision": "2263f4f5cadc4e762114",
+    "url": "/CinemaApp/static/js/main.1df9abe9.chunk.js"
+  },
+  {
+    "revision": "2910247f4a6ead4d81fd",
+    "url": "/CinemaApp/static/js/runtime-main.e7c53bb9.js"
+  }
+]);
