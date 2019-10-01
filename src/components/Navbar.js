d1cd914b64a761d5e8ab34d71ec272cb3452a3d5
Filip <filip.k.wanat@gmail.com>
2019-10-01 17:08:03 +0200

    Change navbar style and add prices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div
-            style={{
-                background: '#ccc',
-                padding: '5px'
-            }}
-        >
+        <div className="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+            <span className="navbar-brand mb-0 h1">
+                Cinema Town <small className="navbar-band">the best cinemas</small>
+            </span>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">
+                            Program
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">
+                            Now in cinemas
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/prices">
+                            Prices
+                        </Link>
+                    </li>
+                </ul>
+            </div>
             <Link to="/"> To Program</Link>
             <Link to="/reservation"> To Reservation</Link>
         </div>
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Program from './Program';
 import Film from './Film';
 import Reservation from './Reservation';
+import Prices from './Prices';
 
 const Router = () => {
     return (
         <Switch className="router">
             <Route path="/" exact component={Program} />
+            <Route path="/prices" exact component={Prices} />
             <Route path="/film/:id" exact component={Film} />
             <Route path="/reservation" exact component={Reservation} />
         </Switch>
