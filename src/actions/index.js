24f852ecf3500d49cfa334908309e4ed2ea442ee
Filip <filip.k.wanat@gmail.com>
2019-10-04 14:11:35 +0200

    Repair cinema update

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,42 +2,49 @@ import _ from 'lodash';
 import omdb from '../apis/omdb';
 import heroku from '../apis/heroku';
 
-// export const fetchFilms = id => async dispatch => _fetchFilms(id, dispatch);
-// const _fetchFilms = _.memoize(async (id, dispatch) => {
-//     const response = await heroku.get('cinemas/' + id);
+export const fetchFilms = (id, clearCache) => async dispatch => {
+    //console.log('cache: ', _fetchFilms.cache, clearCache);
+    if (clearCache === true) {
+        //console.log(' if cache: ', _fetchFilms.cache);
+        _fetchFilms.cache = new _.memoize.Cache();
+        //console.log('cache if: ', _fetchFilms.cache);
+    }
+    return _fetchFilms(id, dispatch);
+};
+
+const _fetchFilms = _.memoize(async (id, dispatch) => {
+    const response = await heroku.get('cinemas/' + id);
 
 //     let array = Array.from(new Set(response.data.map(x => x.movie)));
 
-//     const movies = await Promise.all(
-//         array.map(async movieId => {
-//             console.log("WEJSCIE DO PROMISE ALL");
-//             const hours = response.data.filter(h => h.movie === movieId);
-//             const movie = await heroku.get('movies/' + movieId);
-//             return { movie: movie.data[0], hours: hours.map(x => _.pick(x, ['hour', 'id', 'day'])) };
-//         })
-//     );
+    const movies = await Promise.all(
+        array.map(async movieId => {
+            //console.log('WEJSCIE DO PROMISE ALL');
+            const hours = response.data.filter(h => h.movie === movieId);
+            const movie = await heroku.get('movies/' + movieId);
+            return { movie: movie.data[0], hours: hours.map(x => _.pick(x, ['hour', 'id', 'day'])) };
+        })
+    );
 
 //     dispatch({ type: 'FETCH_FILMS', payload: movies });
 // });
 
+// export const fetchFilms = id => async dispatch => {
+//     const response = await heroku.get('cinemas/' + id);
 
+//         let array = Array.from(new Set(response.data.map(x => x.movie)));
 
-export const fetchFilms = id => async dispatch => {
-    const response = await heroku.get('cinemas/' + id);
+//         const movies = await Promise.all(
+//             array.map(async movieId => {
+//                 console.log("WEJSCIE DO PROMISE ALL");
+//                 const hours = response.data.filter(h => h.movie === movieId);
+//                 const movie = await heroku.get('movies/' + movieId);
+//                 return { movie: movie.data[0], hours: hours.map(x => _.pick(x, ['hour', 'id', 'day'])) };
+//             })
+//         );
 
-        let array = Array.from(new Set(response.data.map(x => x.movie)));
-    
-        const movies = await Promise.all(
-            array.map(async movieId => {
-                console.log("WEJSCIE DO PROMISE ALL");
-                const hours = response.data.filter(h => h.movie === movieId);
-                const movie = await heroku.get('movies/' + movieId);
-                return { movie: movie.data[0], hours: hours.map(x => _.pick(x, ['hour', 'id', 'day'])) };
-            })
-        );
-    
-        dispatch({ type: 'FETCH_FILMS', payload: movies });
-}
+//         dispatch({ type: 'FETCH_FILMS', payload: movies });
+// }
 
 export const fetchFilm = id => async dispatch => _fetchFilm(id, dispatch);
 const _fetchFilm = _.memoize(async (id, dispatch) => {
@@ -47,8 +54,8 @@ const _fetchFilm = _.memoize(async (id, dispatch) => {
 });
 
 export const fetchCinemas = () => async dispatch => {
-   const response = await heroku.get('cinemas/');
-   dispatch({type: 'FETCH_CINEMAS',payload: response.data});
+    const response = await heroku.get('cinemas/');
+    dispatch({ type: 'FETCH_CINEMAS', payload: response.data });
 };
 
 // export const fetchCinemas = () => async dispatch => _fetchCinemas(dispatch);
@@ -57,9 +64,7 @@ export const fetchCinemas = () => async dispatch => {
 //     dispatch({type: 'FETCH_CINEMAS',payload: response.data});
 // })
 
-
-
-export const saveCinema = (id) => dispatch => {
+export const saveCinema = id => dispatch => {
     // if(!id) return null;
-    dispatch({type: 'SAVE_CINEMA',payload: id});
- }
+    dispatch({ type: 'SAVE_CINEMA', payload: id });
+};
